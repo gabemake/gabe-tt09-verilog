3eacf99ca7b08cf5dae4037e8b6e5cb5cbb6a31d
GabeMake <68289201+gabemake@users.noreply.github.com>
2024-11-06 23:27:00 -0800

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 GabeMake
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -23,7 +23,7 @@ module tt_um_example (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-    wire _unused = &{ena, uio_in[7:0], ui_in[1:3], 1'b0};
+  wire _unused = &{ena, uio_in[7:0], ui_in[1:3], 1'b0};
 
   // countdown
   //typedef enum states { IDLE, COUNTING, SCORE1, SCORE2, SCORE3, SCORE4, SCOREB, OVER1, OVER2, OVER3, OVER4, OVERB} states_t;
@@ -37,11 +37,13 @@ module tt_um_example (
   logic [3:0] counter_dig4;
   logic [19:0] counter_ms;
   logic over;
+  
   //typedef enum display {d0, d1, d2, d3, d4, d5, d6, d7, d8, d9, dB, dV, dE, dR, dD} display_t;
   logic [3:0] disp;
 
   always @(posedge clk) begin
     if (!rst_n) begin
+      // reset to IDLE
       next_state = IDLE;
       current_state = IDLE;
       counter_ms = 0;
@@ -55,17 +57,20 @@ module tt_um_example (
       over = 0;
       current_state = next_state;
       if (current_state == IDLE) begin
-          
+        // set up intial game values
         counter_ms = 1000;
-        counter_dig1 = ui_in[4:7];
+        counter_dig1 = ui_in[7:4]; //technically this can be A-F too, but I'm not going to validate.
         counter_dig2 = 9;
         counter_dig3 = 9;
         counter_dig4 = 9;
       end else if (current_state == COUNTING) begin
+        // go digit by digit, decrementing and resetting
         if (counter_ms > 0) begin
+          // count to 1000 cycles every ms
           counter_ms -= 1;
         end else begin
           if (counter_dig4 > 0) begin
+            // do the rest of the digits 1 at a time
             counter_dig4 -= 1;
             counter_ms = 1000;
           end else begin
@@ -87,6 +92,7 @@ module tt_um_example (
                   counter_dig4 = 9;
                   counter_ms = 1000;
                 end else begin
+                  // if dig1 reaches 0, it's game over (yeah I know it's a second behind)
                   over = 1;
                 end
               end
@@ -101,6 +107,7 @@ module tt_um_example (
             counter_ms -= 1;
           end else begin
             counter_ms = 10000000;
+            // count down a full second per OVER/SCORE character
           end
       end
     end
@@ -138,7 +145,7 @@ module tt_um_example (
             next_state = SCORE2;
           else
             next_state = SCORE1;
-          disp = display_t[counter_dig1];
+          disp = display_t[counter_dig1-1]; //account for the fact that the game ends at 1000ms
         end
         SCORE2: begin
           if (counter_ms = 0)
@@ -211,6 +218,7 @@ module tt_um_example (
 
   always_comb begin
     case (disp)
+      //note that the order of segments is .GFEDCBA
       d0: uo_out[7:0] = 8'b00111111;
       d1: uo_out[7:0] = 8'b00000110;
       d2: uo_out[7:0] = 8'b01011011;
@@ -222,11 +230,11 @@ module tt_um_example (
       d8: uo_out[7:0] = 8'b01111111;
       d9: uo_out[7:0] = 8'b01101111;
       dB: uo_out[7:0] = 8'b00000000;
-      dV: uo_out[7:0] = 8'b00111110;
+      dV: uo_out[7:0] = 8'b00111110; //going with the uppercase here
       dE: uo_out[7:0] = 8'b01111001;
       dR: uo_out[7:0] = 8'b01010000;
       dD: uo_out[7:0] = 8'b01000000;
-      default: uo_out[7:0] = 8'b10000000;
+      default: uo_out[7:0] = 8'b10000000; // invalid characters get the .
     endcase
   end
           
